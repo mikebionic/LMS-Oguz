259144075f99a22faa81ff22f477df2698452e73
mike <muhammedjepbarov@gmail.com>
2020-09-14 12:48:53 +0500

    added index page and other modules

--- routes.py
+++ routes.py
@@ -62,6 +62,49 @@ class Attachments(db.Model):
 ###  end of lesson models ###
 
 
+###### library and homeworks ######
+
+@app.route('/')
+def home_page():
+	return render_template('library/index.html')
+
+@app.route('/projects')
+def projects_page():
+	return render_template('library/projects.html')
+
+@app.route('/tutorials')
+def tutorials_page():
+	return render_template('library/tutorials.html')
+
+@app.route('/list')
+def lists_page():
+	return render_template('library/list.html')
+
+@app.route("/library",methods=['GET','POST'])
+def library():
+	return render_template('library/library.html')
+
+@app.route("/add_library_book",methods=['GET','POST'])
+@login_required
+def add_library_book():
+	if(current_user.user_type!="teacher"):
+		flash('Siz shu penjira girip bilenzok!')
+		return redirect("/login")
+	return "add book"
+
+@app.route("/homeworks",methods=['GET','POST'])
+@login_required
+def homeworks():
+	if(current_user.user_type!="teacher"):
+		flash('Siz shu penjira girip bilenzok!')
+		return redirect("/login")
+	return "Check the homeworks"
+
+###############################
+
+#########
+
+
 #### interactive pages of view ####
 
 def save_attachment(form_attachment):
@@ -229,39 +272,6 @@ def sort_lessons():
 			redirect("/student")
 
 #############################
-
-
-
-
-###### library and homeworks ######
- # <a href="/library" class="btn btn-primary">Kitaphana</a>
- #  <a href="/add_library_book" class="btn btn-outline-primary">Kitap goş</a>
- #  <a href="/homeworks" 
-
-@app.route("/library",methods=['GET','POST'])
-def library():
-	return "welcome to lib"
-
-@app.route("/add_library_book",methods=['GET','POST'])
-@login_required
-def add_library_book():
-	if(current_user.user_type!="teacher"):
-		flash('Siz shu penjira girip bilenzok!')
-		return redirect("/login")
-	return "add book"
-
-@app.route("/homeworks",methods=['GET','POST'])
-@login_required
-def homeworks():
-	if(current_user.user_type!="teacher"):
-		flash('Siz shu penjira girip bilenzok!')
-		return redirect("/login")
-	return "Check the homeworks"
-
-###############################
-
-
-
 
 
 ######## admin page functions ########
